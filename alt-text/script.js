ad5011cbb13e40e438c7b0bddd6af45d97eca8ac
Henna Kalliokoski <varpushaukka@users.noreply.github.com>
2025-05-13 17:11:19 +0300

    Update script.js

--- alt-text/script.js
+++ alt-text/script.js
@@ -11,33 +11,33 @@ const exerciseIntroductionText = document.getElementById("exercise-introduction-
 const exercisePage = document.getElementById("exercise-webpage")
 
 instructionsToggleButton.onclick = () => {
-  if (instructionsToggleButton.innerHTML === '+') {
-    instructionsToggleButton.innerHTML = '-'
+  if (instructionsToggleButton.innerHTML === '+ Instructions') {
+    instructionsToggleButton.innerHTML = '- Instructions'
     instructions.style.maxHeight = '5000px'
   } else {
-    instructionsToggleButton.innerHTML = '+'
+    instructionsToggleButton.innerHTML = '+ Instructions'
     instructions.style.maxHeight = '0'
   }
 }
 
 studyMaterialToggleButton.onclick = () => {
-  if (studyMaterialToggleButton.innerHTML === '+') {
-    studyMaterialToggleButton.innerHTML = '-'
+  if (studyMaterialToggleButton.innerHTML === '+ Study Material') {
+    studyMaterialToggleButton.innerHTML = '- Study Material'
     studyMaterial.style.maxHeight = '5000px'
   } else {
-    studyMaterialToggleButton.innerHTML = '+'
+    studyMaterialToggleButton.innerHTML = '+ Study Material'
     studyMaterial.style.maxHeight = '0'
   }
 }
 
 exerciseToggleButton.onclick = () => {
-  if (exerciseToggleButton.innerHTML === '+') {
-    exerciseToggleButton.innerHTML = '-'
+  if (exerciseToggleButton.innerHTML === '+ Exercise') {
+    exerciseToggleButton.innerHTML = '- Exercise'
     exercisePage.style.maxHeight = '2000px'
     exercisePage.style.border = '1px solid black'
     exerciseIntroductionText.style.maxHeight = '1000px'
   } else {
-    exerciseToggleButton.innerHTML = '+'
+    exerciseToggleButton.innerHTML = '+ Excercise'
     exercisePage.style.maxHeight = '0'
     exercisePage.style.border = '0'
     exerciseIntroductionText.style.maxHeight = '0'
